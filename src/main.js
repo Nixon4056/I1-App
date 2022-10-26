5cd8f065aa2a397fc859aaa34b566cbb7f8ba60c
NikodemLaptop <nikodem@nicode.pl>
2022-10-26 10:05:36 +0200

    crashed

--- src/main.js
+++ src/main.js
@@ -3,21 +3,7 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
-//COMPONENTS
-import BaseKanban from './components/UI/BaseKanban.vue'
-
-//SIMPLE COMPONENTS
-import LoginBtn from './components/layout/TheLoginBtn.vue'
-import UserIcon from './components/svg/UserIcon.vue'
-import navIcons from './components/svg/NavIcons.vue'
-import TheLogo from './components/svg/TheLogo.vue'
-
-const app = createApp(App);
-app.component('base-kanban', BaseKanban)
-app.component('login-btn', LoginBtn)
-app.component('user-icon', UserIcon)
-app.component('nav-icons', navIcons)
-app.component('the-logo', TheLogo)
-app.use(store)
-app.use(router)
-app.mount('#app')
+createApp(App)
+  .use(router)
+  .use(store)
+  .mount('#app')
