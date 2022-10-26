5d6bae3bbf7b770cd8e1387c9f14b5ff3159aa89
Nikodem <nikodem@nicode.pl>
2022-10-26 22:13:05 +0200

    error

--- src/views/TheIt.vue
+++ src/views/TheIt.vue
@@ -5,69 +5,41 @@
         <router-link to="/dashboard">Informatyk1 Dashboard</router-link> / IT
       </template>
       <template #kanban>
-        <base-column 
-          @drop="onDrop($event, 1)"
-          @dragenter.prevent
-          @dragover.prevent
-          title="PRZYJĘTE">
-          <the-task
-            v-for="task in filteredTasks(1)"
-            :draggable="true"
-            @dragstart="startDrag($event, task)"
-            :key="task.id"
-            :company="task.company"
-            :description="task.description"
-            :employee="task.employee"
+        <base-column title="PRZYJĘTE">
+          <draggable
+            tag="ul"
+            ghost-class="moving-card"
+            :list="filteredTasks(1)"
+            :group="tasks"
+            :animation="200"
           >
-          </the-task>
+            <the-task
+              v-for="task in filteredTasks(1)"
+              :key="task.id"
+              :company="task.company"
+              :description="task.description"
+              :employee="task.employee"
+            >
+            </the-task>
+          </draggable>
         </base-column>
-        <base-column 
-          @drop="onDrop($event, 2)"
-          @dragenter.prevent
-          @dragover.prevent
-          title="W REALIZACJI">
-          <the-task
-            v-for="task in filteredTasks(2)"
-            :draggable="true"
-            @dragstart="startDrag($event, task)"
-            :key="task.id"
-            :company="task.company"
-            :description="task.description"
-            :employee="task.employee"
+        <base-column title="PRZYJĘTE2">
+          <draggable
+            tag="ul"
+            ghost-class="moving-card"
+            :list="filteredTasks(2)"
+            :group="tasks"
+            :animation="200"
           >
-          </the-task>
-        </base-column>
-        <base-column 
-          @drop="onDrop($event, 3)"
-          @dragenter.prevent
-          @dragover.prevent
-          title="ZREALIZOWANO">
-          <the-task
-            v-for="task in filteredTasks(3)"
-            :draggable="true"
-            @dragstart="startDrag($event, task)"
-            :key="task.id"
-            :company="task.company"
-            :description="task.description"
-            :employee="task.employee"
-          >
-          </the-task>
-        </base-column>
-        <base-column 
-          @drop="onDrop($event, 4)"
-          @dragenter.prevent
-          @dragover.prevent
-          title="ODŁOŻONO">
-          <the-task
-            v-for="task in filteredTasks(4)"
-            :draggable="true"
-            @dragstart="startDrag($event, task)"
-            :key="task.id"
-            :company="task.company"
-            :description="task.description"
-            :employee="task.employee"
-          >
-          </the-task>
+            <the-task
+              v-for="task in filteredTasks(2)"
+              :key="task.id"
+              :company="task.company"
+              :description="task.description"
+              :employee="task.employee"
+            >
+            </the-task>
+          </draggable>
         </base-column>
       </template>
     </base-kanban>
@@ -76,48 +48,56 @@
 </template>
 
 <script>
+import Draggable from 'vuedraggable';
 export default {
+  components: {
+    Draggable,
+  },
   data() {
     return {
       tasks: [],
+      newTasks: [],
     };
   },
   methods: {
     filteredTasks(status) {
       return this.tasks.filter((task) => {
-        if(task.status === status){
-          return true
+        if (task.status === status) {
+          return true;
         }
-        if(task.status === status){
-          return true
+        if (task.status === status) {
+          return true;
         }
-        if(task.status === status){
-          return true
+        if (task.status === status) {
+          return true;
         }
-        if(task.status === status){
-          return true
+        if (task.status === status) {
+          return true;
         }
-        return false
-      })
+        return false;
+      });
     },
-    startDrag(event, item){
-      console.log(event)
-      event.target.style.backgroundColor = 'red'
-      event.dataTransfer.dropEffect = 'move'
-      event.dataTransfer.effectAllowed = 'move'
-      event.dataTransfer.setData('itemId', item.id)
+    /*     startDrag(event, item) {
+      console.log(event);
+      event.target.style.backgroundColor = 'red';
+      event.dataTransfer.dropEffect = 'move';
+      event.dataTransfer.effectAllowed = 'move';
+      event.dataTransfer.setData('itemId', item.id);
     },
-    onDrop(event, status){
+    onDrop(event, status) {
       const itemId = event.dataTransfer.getData('itemId');
       const item = this.tasks.find((task) => task.id == itemId);
       item.status = status;
-    }
+    }, */
   },
   created() {
     this.tasks = this.$store.getters['tasks/tasks'];
-    console.log(this.tasks)
   },
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.moving-card {
+  background-color: black;
+}
+</style>
